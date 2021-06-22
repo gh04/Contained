e1951b1afb3bff646198870621e1f8f4e9fbf341
Gerardo Hernandez <gahernandez02@gmail.com>
2021-06-22 15:21:02 -0700

    Grouped files by type and built the info sequence on IB.

---
+++ Contained/Contained/View Contollers/InfoViewController.swift
@@ -0,0 +1,36 @@
+//
+//  InfoViewController.swift
+//  Contained
+//
+//  Created by Gerardo Hernandez on 6/22/21.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    // MARK: -
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - IBActions
+    @IBAction func done(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
