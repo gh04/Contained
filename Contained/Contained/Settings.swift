7e19485c4658c97f440751234e369cae1fcf6133
Gerardo Hernandez <gahernandez02@gmail.com>
2021-06-21 19:00:31 -0700

    Added a Settings file for the model

---
+++ Contained/Contained/Settings.swift
@@ -0,0 +1,16 @@
+//
+//  Settings.swift
+//  Contained
+//
+//  Created by Gerardo Hernandez on 6/21/21.
+//
+
+import Foundation
+
+class Settings {
+    static let shared = Settings()
+    private init() {}
+    
+    var shouldRoll = false
+    var shouldZoom = false
+}
